f78927c202c6f9b6843f76fa6a8940a988196e16
Julia Löschel <juloeschel@gmail.com>
2022-03-04 22:28:17 +0100

    update fetch to async await

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,15 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Fetch() {
+  const [data, setData] = useState(["one", "two"]);
   useEffect(() => {
-    fetch("https://fetch-me.vercel.app/api/shopping/items")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    fetchData();
   }, []);
-
+  console.log(data);
   return (
     <>
       <span>fetch successfull</span>
     </>
   );
+
+  async function fetchData() {
+    const response = await fetch(
+      "https://fetch-me.vercel.app/api/shopping/items"
+    );
+    const newdata = await response.json();
+    if (response.status === 200) {
+      setData(newdata.data);
+    } else {
+      console.error("fetch went wrong");
+    }
+  }
 }
