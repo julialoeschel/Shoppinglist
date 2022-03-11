9dc685ab5c7053862829b8b81e6aba5b84068350
Julia Löschel <juloeschel@gmail.com>
2022-03-11 14:36:13 +0100

    update categories

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     setShoppingList(filteredList);
   }
 
-  // console.log(shoppingList);
+  console.log(shoppingList);
 
   return (
     <>
--- src/ShoppingList/ShoppingList.js
+++ src/ShoppingList/ShoppingList.js
@@ -2,18 +2,48 @@ import ListElement from "../ListElement/Listelement";
 import "./shoppingList.css";
 
 export default function ShoppingList({ List, onDelete }) {
+  let fruits = [];
+  let bread = [];
+
+  List.forEach((element) => {
+    if (element.category._ref === "c2hvcHBpbmcuY2F0ZWdvcnk6MA==") {
+      fruits.push(element);
+    }
+    if (element.category._ref === "c2hvcHBpbmcuY2F0ZWdvcnk6MQ==") {
+      bread.push(element);
+    }
+  });
+
   return (
-    <>
+    <div className="list">
       <h2>Shoppinglist</h2>
-      <div class="shoppingItemsContainer">
-        {List.map((item) => (
-          <ListElement
-            key={item._id}
-            item={item}
-            onTab={() => onDelete(item._id)}
-          ></ListElement>
-        ))}
+      <div className="container">
+        {fruits.length > 0 && (
+          <div class="shoppingItemsContainer">
+            <h3>Obst und Gemüse</h3>
+            {fruits.map((item) => (
+              <ListElement
+                key={item._id}
+                item={item}
+                onTab={() => onDelete(item._id)}
+              ></ListElement>
+            ))}
+          </div>
+        )}
+
+        {bread.length > 0 && (
+          <div class="shoppingItemsContainer">
+            <h3>Brot</h3>
+            {bread.map((item) => (
+              <ListElement
+                key={item._id}
+                item={item}
+                onTab={() => onDelete(item._id)}
+              ></ListElement>
+            ))}
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
